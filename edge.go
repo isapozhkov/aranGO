1ac7ca39fa4227e53d24ade244748bb9f984c57a
i.sapozhkov <i.sapozhkov@rsc-tech.ru>
2017-07-27 12:33:36 +0300

    Add edges parsing interface into arguments

--- edge.go
+++ edge.go
@@ -1,36 +1,36 @@
 package aranGO
 
-import "errors"
+import (
+	"encoding/json"
+	"errors"
+)
 
 type Edge struct {
-	Id		string	`json:"_id,omitempty"`
-	Key		string	`json:"_key,omitempty"`
-	Rev		string	`json:"_rev,omitempty"`
-	Oldrev	string	`json:"_oldRev,omitempty"`
-	From	string	`json:"_from"`
-	To		string	`json:"_to"`
-	Label	string	`json:"$label,omitempty"` // Not realized in v3.2
-
-	Error   bool	`json:"error,omitempty"`
-	Message string	`json:"errorMessage,omitempty"`
+	Id     string `json:"_id,omitempty"`
+	Key    string `json:"_key,omitempty"`
+	Rev    string `json:"_rev,omitempty"`
+	Oldrev string `json:"_oldRev,omitempty"`
+	From   string `json:"_from"`
+	To     string `json:"_to"`
+	Label  string `json:"$label,omitempty"` // Not realized in v3.2
 }
 
 type edgeStats struct {
-	Scanned		int	`json:"scannedIndex"`
-	Filtered	int	`json:"filtered"`
+	Scanned  int `json:"scannedIndex"`
+	Filtered int `json:"filtered"`
 }
 
 type edgesResponce struct {
-	Edges	[]Edge		`json:"edges"`
-	Stats	edgeStats	`json:"stats"`
-	
-	Code	int			`json:"code"`
-	Error   bool		`json:"error"`
+	Edges []Edge    `json:"edges"`
+	Stats edgeStats `json:"stats"`
 
+	Code  int  `json:"code"`
+	Error bool `json:"error"`
 }
-// Read in- or outbound edges
+
+// Edges read in- or outbound edges
 // GET /_api/edges/{collection-id}
-func (db *Database) Edges(edgeCol string, startVertex string, direction string) ([]Edge, error) {
+func (db *Database) Edges(edgeCol string, startVertex string, direction string, edges interface{}) ([]Edge, error) {
 	if edgeCol == "" || startVertex == "" {
 		return nil, errors.New("Invalid key or collection")
 	}
@@ -49,6 +49,10 @@ func (db *Database) Edges(edgeCol string, startVertex string, direction string) 
 
 	switch res.Status() {
 	case 200:
+		if edges != nil {
+			b, _ := json.Marshal(eresp.Edges)
+			json.Unmarshal(b, edges)
+		}
 		return eresp.Edges, nil
 	case 400:
 		return nil, errors.New("Request contains invalid parameters")
